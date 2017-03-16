cb500b16ed15bd7edf2b2e671066ef605f9ca93f
Christian Hülsmann <christian_huelsmann@gmx.de>
2017-03-16 12:58:50 +0100

    Initial commit

---
+++ MD5.c
@@ -0,0 +1,63 @@
+/**
+ * @file MD5.c
+ * @date 11.03.2017
+ * @author Christian Hülsmann (christian_huelsmann@gmx.de)
+ */
+
+#include "MD5.h"
+#include <Wincrypt.h>
+
+
+BOOLEAN MD5_Init(MD5_Context* Context)
+{
+	if (!CryptAcquireContext(&Context->hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+	{
+		return FALSE;
+	}
+	if (!CryptCreateHash(Context->hProv, CALG_MD5, 0, 0, &Context->hHash))
+	{
+		CryptReleaseContext(Context->hProv, 0);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+BOOLEAN MD5_Update(MD5_Context* Context, const PBYTE Buf, DWORD BufSize)
+{
+	if (!CryptHashData(Context->hHash, Buf, BufSize, 0))
+	{
+		CryptReleaseContext(Context->hProv, 0);
+		CryptDestroyHash(Context->hHash);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+BOOLEAN MD5_Final(MD5_Context* Context)
+{
+	BOOLEAN ret = FALSE;
+	DWORD hashLen = MD5_DIGEST_SIZE;
+
+	if (CryptGetHashParam(Context->hHash, HP_HASHVAL, Context->digest, &hashLen, 0))
+	{
+		ret = TRUE;
+	}
+	CryptDestroyHash(Context->hHash);
+	CryptReleaseContext(Context->hProv, 0);
+
+	return ret;
+}
+
+BOOLEAN MD5_Calculate(const PBYTE Buf, DWORD BufSize, PBYTE Digest)
+{
+	MD5_Context context;
+	ZeroMemory(&context, sizeof(context);
+	
+	if (!Init(&context) || !Update(&context, Buf, BufSize) || !Final(&context))
+	{
+		return FALSE;
+	}
+	
+	CopyMemory(Digest, context.digest, MD5_DIGEST_SIZE);
+	return TRUE;
+}
---
+++ MD5.h
@@ -0,0 +1,59 @@
+/**
+ * @file MD5.h
+ * @date 11.03.2017
+ * @author Christian Hülsmann (christian_huelsmann@gmx.de)
+ */
+
+#ifndef MD5_H
+#define MD5_H
+
+#include <Windows.h>
+
+// Size of an MD5 digest in bytes
+#define MD5_DIGEST_SIZE 16
+
+
+// Context structure for MD5 hashing
+struct MD5_Context
+{
+	HCRYPTPROV hProv;
+	HCRYPTHASH hHash;
+	BYTE digest[MD5_DIGEST_SIZE];
+};
+
+
+/**
+ * Initializes the MD5_Context structure for calculating MD5 hashes.
+ * @param	Context	MD5_Context structure to initialize
+ * @return			TRUE if successfull, otherwise FALSE
+ */
+BOOLEAN MD5_Init(MD5_Context* Context);
+
+/**
+ * Hashes data in order to calculate and MD5 hash.
+ * @param	Context	MD5_Context structure
+ * @param	Buf		Data to hash
+ * @param	BufSize	Size of the buffer Buf
+ * @return			TRUE if successfull, otherwise FALSE
+ */
+BOOLEAN MD5_Update(MD5_Context* Context, const PBYTE Buf, DWORD BufSize);
+
+/**
+ * Finalizes the MD5 hash.
+ * @param	Context	MD5_Context structure
+ * @return			TRUE if successfull, otherwise FALSE
+ */
+BOOLEAN MD5_Final(MD5_Context* Context);
+
+/**
+ * Calculates an MD5 hash of a buffer in one pass.
+ * @param	Buf		Buffer to calculate the hash for
+ * @param	BufSize	Size of the buffer Buf
+ * @param	Digest	Byte array which receives the MD5 digest if the function succeeds,
+ *					it has to be at least of size MD5_DIGEST_SIZE
+ * @return			TRUE if successfull, otherwise FALSE
+ */
+BOOLEAN MD5_Calculate(const PBYTE Buf, DWORD BufSize, PBYTE Digest);
+
+
+#endif // #ifndef MD5_H
